127bfabc15d9b80277454cfe9983c0980cbed9af
Dinithi Yapa <136271101+Dinithiyapa@users.noreply.github.com>
2024-12-10 09:28:52 +0530

    Add API integration to web app

--- web/app.js
+++ web/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const fetch = require('node-fetch'); // Added to make HTTP requests to the API
 
 var routes = require('./routes/index');
 
@@ -14,12 +15,28 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Define API URL (ensure this points to your API container's URL)
+const apiUrl = process.env.API_URL || 'http://172.235.1.190:8080'; // Adjust to match API container
+
+// Fetch status from the API
+app.get('/status', (req, res) => {
+  fetch(`${apiUrl}/api/status`) // API call to the API container
+    .then(response => response.json())
+    .then(data => {
+      res.render('status', { data: data }); // Render status in the view
+    })
+    .catch(error => {
+      console.error('Error fetching API:', error);
+      res.status(500).json({ message: 'Error connecting to API' });
+    });
+});
 
 app.use('/', routes);
 
@@ -37,7 +54,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render({
+    res.render('error', {
       message: err.message,
       error: err
     });
@@ -48,11 +65,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render({
+  res.render('error', {
     message: err.message,
     error: {}
   });
 });
-
 
 module.exports = app;
