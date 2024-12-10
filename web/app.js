6706894bc945925c31f19aa0203e7a88d89c492e
Dinithi Yapa <136271101+Dinithiyapa@users.noreply.github.com>
2024-12-10 10:20:00 +0530

    Update app.js

--- web/app.js
+++ web/app.js
@@ -1,48 +1,31 @@
-import express from 'express';
-import path from 'path';
-import favicon from 'serve-favicon';
-import logger from 'morgan';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import fetch from 'node-fetch'; // Use import for node-fetch (ESM)
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-import routes from './routes/index';
+var routes = require('./routes/index');
 
-const app = express();
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define API URL (ensure this points to your API container's URL)
-const apiUrl = process.env.API_URL || 'http://172.235.1.190:8080'; // Adjust to match API container
-
-// Fetch status from the API
-app.get('/status', (req, res) => {
-  fetch(`${apiUrl}/api/status`) // API call to the API container
-    .then(response => response.json())
-    .then(data => {
-      res.render('status', { data: data }); // Render status in the view
-    })
-    .catch(error => {
-      console.error('Error fetching API:', error);
-      res.status(500).json({ message: 'Error connecting to API' });
-    });
-});
-
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -52,9 +35,9 @@ app.use((req, res, next) => {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render({
       message: err.message,
       error: err
     });
@@ -63,12 +46,13 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use((err, req, res, next) => {
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render({
     message: err.message,
     error: {}
   });
 });
 
-export default app;
+
+module.exports = app;
