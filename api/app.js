604191e22f66a9b2de947765ee1b680ba9af1624
Dinithi Yapa <136271101+Dinithiyapa@users.noreply.github.com>
2024-12-10 10:30:15 +0530

    Define API URL

--- api/app.js
+++ api/app.js
@@ -5,6 +5,8 @@ var uuid = require('node-uuid');
 var pg = require('pg');
 var conString = process.env.DB; // "postgres://username:password@localhost/database";
 
+// Define API URL (ensure this points to your API container's URL)
+const apiUrl = process.env.API_URL || 'http://172.235.1.190:8080'; // Adjust to match API container
 // Routes
 
 // Root route
