c5ab36238b3b2444c309725a20ffd85fa0d77e0c
Dinithi Yapa <136271101+Dinithiyapa@users.noreply.github.com>
2024-12-11 05:49:07 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -22,3 +22,23 @@ docker rm buzbud-db-action
 docker pull yapadinithi/buzbud-web-action:v1.0.0
 docker pull yapadinithi/buzbud-api-action:v1.0.0
 docker pull postgres:latest  # Pull the latest PostgreSQL image
+
+# Run the new containers with the environment variables from .env
+docker run -d \
+  -p 3000:3000 \
+  --name buzbud-web-action \
+  --env-file .env \
+  yapadinithi/buzbud-web-action:v1.0.0
+
+docker run -d \
+  -p 4000:4000 \
+  --name buzbud-api-action \
+  --env-file .env \
+  yapadinithi/buzbud-api-action:v1.0.0
+
+docker run -d \
+  -p 5432:5432 \
+  --name buzbud-db-action \
+  -e POSTGRES_USER=$POSTGRES_USER \
+  -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+  postgres:latest
