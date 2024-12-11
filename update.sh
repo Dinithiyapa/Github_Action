75966d63e58578f0f420ae21dcf53298989c2444
Dinithi Yapa <136271101+Dinithiyapa@users.noreply.github.com>
2024-12-11 05:54:29 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Load environment variables from the .env file
-if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs)
-else
-  echo ".env file not found! Exiting..."
-  exit 1
-fi
-
 # Stop the currently running containers
 docker stop buzbud-web-action
 docker stop buzbud-api-action
@@ -21,24 +13,29 @@ docker rm buzbud-db-action
 # Pull the latest Docker images
 docker pull yapadinithi/buzbud-web-action:v1.0.0
 docker pull yapadinithi/buzbud-api-action:v1.0.0
-docker pull postgres:latest  # Pull the latest PostgreSQL image
+docker pull postgres:latest
 
-# Run the new containers with the environment variables from .env
+# Run the new containers with the correct port mapping and environment variables
+
+# Run the web container
 docker run -d \
   -p 3000:3000 \
   --name buzbud-web-action \
-  --env-file .env \
+  --env API_URL=http://172.235.1.190:4000 \
   yapadinithi/buzbud-web-action:v1.0.0
 
+# Run the API container
 docker run -d \
   -p 4000:4000 \
   --name buzbud-api-action \
-  --env-file .env \
+  --env API_DB=postgres://user1:password1@172.235.1.190:5432/busbud_db \
+  --env API_URL=http://172.235.1.190:4000 \
   yapadinithi/buzbud-api-action:v1.0.0
 
+# Run the database container
 docker run -d \
   -p 5432:5432 \
   --name buzbud-db-action \
-  -e POSTGRES_USER=$POSTGRES_USER \
-  -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+  -e POSTGRES_USER=user1 \
+  -e POSTGRES_PASSWORD=password1 \
   postgres:latest
